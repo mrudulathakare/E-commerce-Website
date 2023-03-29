83460a3d11d5f9f389acdebadbb8e4e986634cd8
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-29 23:13:34 +0530

    Working on SEO

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Footer from './Footer';
 import Header from './Header';
-
-const Layout = ({children}) => {
+import { Helmet } from 'react-helmet';
+ 
+const Layout = ({children,title,description,keywords,author}) => {
   return (
     <div>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content={author} />
+          <title>{title}</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
         <Header/>
         <main style={{minHeight:"73.8vh"}}>
         {children}
@@ -14,4 +23,10 @@ const Layout = ({children}) => {
   );
 };
 
+Layout.defaultProps = {
+  title: "i2Eye - Shop Now",
+  description: "mern stack project",
+  keywords: "mern,react,node,mongoDB",
+  author: "Nirali Mamaniya"
+}
 export default Layout;

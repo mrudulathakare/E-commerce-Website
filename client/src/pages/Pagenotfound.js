1cfdf795a3d46db6b7b68609a8536df408bc97ae
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-29 23:14:51 +0530

    Working on SEO - adding title to each file

--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout/Layout'
 import {Link} from "react-router-dom";
 const Pagenotfound = () => {
   return (
-    <Layout>
+    <Layout title={"Page Not Found - i2Eye"}>
         <div className="pnf">
           <h1 className="pnf-title">404</h1>
           <h2 className="pnf-heading">Oops ! Page Not Found</h2>

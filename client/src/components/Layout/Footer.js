47d4d5cc3b813428a8af1d7cc1e44085a47f4199
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-29 18:46:08 +0530

    Added styling and text to the footer

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-        <h1>Footer</h1>
+    <div className="bg-dark text-light p-3">
+        <h4 className="text-center">
+            All Right Reserved &copy; Ecommerce
+        </h4>
     </div>
   )
 }

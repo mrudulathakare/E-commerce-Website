e3521d29458ba7851bbf27ecc451f4278bc31306
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-29 21:47:20 +0530

    Applied css styling and content to header and footer

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
 const Footer = () => {
   return (
-    <div className="bg-dark text-light p-3">
+    <div className="footer">
         <h4 className="text-center">
-            All Right Reserved &copy; Ecommerce
+            All Right Reserved &copy; i2Eye
         </h4>
+        <p className="text-center mt-3">
+          <Link to="/about">About</Link>|
+          <Link to="/contact">Contact</Link>|
+          <Link to="/policy">Privacy Policy</Link>
+        </p>
     </div>
   )
 }

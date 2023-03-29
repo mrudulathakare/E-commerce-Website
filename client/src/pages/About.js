c1b5c26a3c263c69e69ecc91b66349b52e55c2b8
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-29 22:36:11 +0530

    Added content in about,contactus,privacy policy sections

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,7 +4,23 @@ import Layout from './../components/Layout/Layout';
 const About = () => {
   return (
     <Layout>
-        <h1>AboutPage</h1>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.png"
+            alt="about"
+            style={{ width: "80%"}}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+          &emsp;&emsp;i2Eye is an internet-first brand of eyewear for people around the world. The product catalog includes customizable eyeglasses, prescription glasses, and more.
+          <br></br><br></br>
+          <h4>Vision</h4>
+          &emsp;&emsp;<i>Creating a sharper world by ensuring that every individual can see better and live better.</i>
+          </p>
+        </div>
+      </div>
     </Layout>
   )
 }
--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -4,7 +4,18 @@ import Layout from './../components/Layout/Layout';
 const Policy = () => {
   return (
     <Layout>
-        <h1>Privacy Policy Page</h1>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contactme.jpg"
+            alt="contactus"
+            style={{ width: "90%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>&emsp;&emsp;This Privacy Policy is applicable on You when You access and use Our Website/Stores. We reserve the right to modify or amend this Privacy Policy at any time and for any reason. However, We commit to ensuring that the privacy rights of individuals shall be maintained.<br></br><br></br>BY USING OUR SERVICES OR BY OTHERWISE GIVING US YOUR PERSONAL INFORMATION, YOU AGREE TO THE TERMS OF THIS PRIVACY POLICY.</p>
+        </div>
+      </div>
     </Layout>
   )
 }

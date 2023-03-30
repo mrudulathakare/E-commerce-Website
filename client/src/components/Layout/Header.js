b65c00e5aac34ef53ec3e3b01fdfbd825bcc8e26
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-30 19:00:02 +0530

    Protected navbar using context-api

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import {NavLink, Link} from 'react-router-dom';
 import {Gi3DGlasses} from 'react-icons/gi';
+import { useAuth } from '../../context/auth';
+import toast from 'react-hot-toast';
 const Header = () => {
+  const [auth,setAuth] = useAuth();
+  const handleLogout = () => {
+    setAuth({
+      ...auth,
+      user:null,
+      token:''
+    });
+    localStorage.removeItem('auth');
+    toast.success("Logout Successfully");
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -28,7 +41,9 @@ const Header = () => {
             Category
           </NavLink >
         </li>
-        <li className="nav-item">
+        {
+          !auth.user ? (<>
+          <li className="nav-item">
           <NavLink to="/register" className="nav-link">
             Register
           </NavLink >
@@ -38,6 +53,14 @@ const Header = () => {
             Login
           </NavLink >
         </li>
+          </>) : (<>
+            <li className="nav-item">
+          <NavLink onClick={handleLogout} to="/login" className="nav-link">
+            Log out
+          </NavLink >
+          </li>
+          </>)
+        }
         <li className="nav-item">
           <NavLink to="/cart" className="nav-link">
             Cart (0)

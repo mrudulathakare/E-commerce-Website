9b343e6b72f0bbd4a659cd349d5911836994375f
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-30 11:53:02 +0530

    Updated authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,28 +7,28 @@ export const registerController = async(req,res) => {
 
         // validations
         if(!name){
-            return res.send({error:"Name is required"})
+            return res.send({message:"Name is required"})
         }
         if(!email){
-            return res.send({error:"Email is required"})
+            return res.send({message:"Email is required"})
         }
         if(!password){
-            return res.send({error:"Password is required"})
+            return res.send({message:"Password is required"})
         }
         if(!phone){
-            return res.send({error:"Phone number is required"})
+            return res.send({message:"Phone number is required"})
         }
         if(!address){
-            return res.send({error:"Address is required"})
+            return res.send({message:"Address is required"})
         }
 
         // check user
         const existingUser = await userModel.findOne({email})
 
-        // existing use
+        // existing user
         if(existingUser){
             return res.status(200).send({
-                success:true,
+                success:false,
                 message:"Already registered please login",
             })
         }
@@ -37,35 +37,35 @@ export const registerController = async(req,res) => {
         const hashedPassword = await hashPassword(password)
 
         // to save
-        const user = await new userModel({name,email,phone,address,password:hashedPassword}).save()
+        const user = await new userModel({name,email,phone,address,password:hashedPassword}).save();
 
         res.status(201).send({
             success:true,
             message:"User Register Successfully",
             user,
-        })
+        });
     }catch(error){
         console.log(error)
         res.status(500).send({
             success:false,
             message:"Error in Registration",
-            error
-        })
+            error,
+        });
     }
-}
+};
 
 
 // POST LOGIN
 
 export const loginController = async(req,res) => {
     try{
-        const {email,password} = req.body
+        const {email,password} = req.body;
         // validations
         if(!email || !password){
             return res.status(404).send({
                 success:false,
                 message:"Invalid email or password"
-            })
+            });
         }
 
         // check user
@@ -74,7 +74,7 @@ export const loginController = async(req,res) => {
             return res.status(404).send({
                 success:false,
                 messae:"Email is not registered"
-            })
+            });
         }
         const match = await comparePassword(password,user.password)
 
@@ -111,6 +111,11 @@ export const loginController = async(req,res) => {
 
 // test controller
 export const testController = (req,res) => {
-    res.send("Protected route");
+    try{
+        res.send("Protected route");
+    }catch(error){
+        console.log(error);
+        res.send({error});
+    }
 }
 

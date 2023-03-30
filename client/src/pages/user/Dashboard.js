f4bd6d3d908da13822d551531e486f562b72ce6b
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-30 19:10:37 +0530

    Created a folder 'user' with 'Dashboard.js' file

---
+++ client/src/pages/user/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './../../components/Layout/Layout';
+
+const Dashboard = () => {
+  return (
+    <Layout title = 'Dashboard - i2Eye'>
+        <h1>Dashboard Page</h1>
+    </Layout>
+    
+  )
+}
+
+export default Dashboard

c8606196c63c6638443e0ad645354ebe50dfd9f3
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-03-31 09:15:45 +0530

    Created sample dashboard for user

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import Layout from './../../components/Layout/Layout';
-
+import React from "react";
+import Layout from "../../components/Layout/Layout";
 const Dashboard = () => {
   return (
-    <Layout title = 'Dashboard - i2Eye'>
-        <h1>Dashboard Page</h1>
+    <Layout title={"Dashboard - i2Eye"}>
+      <h1>Dashborad Page</h1>
     </Layout>
-    
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;

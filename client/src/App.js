5c6fb6f12c7bcdbf3b5ea0cbf08daa75320d3180
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-04-02 10:46:44 +0530

    Added new route for forgot password

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,20 @@ import Policy from './pages/Policy';
 import Pagenotfound from './pages/Pagenotfound';
 import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login';
-import Dashboard from './pages/user/Dashboard';
-import PrivateRoute from './components/Routes/Private';
+import Dashboard from './pages/user/Dashboard'
+// import PrivateRoute from './components/Routes/Private';
+import ForgotPassword from './pages/Auth/ForgotPassword';
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/dashboard" element={<PrivateRoute />}>
-          <Route path="" element={<Dashboard />} />
-        </Route>
+        <Route path="/dashboard" element={<Dashboard/>} />
+        {/* <Route path="/dashboard" element={<PrivateRoute/>}>
+          <Route path="" element={<Dashboard/>} />
+        </Route> */}
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
@@ -26,5 +29,4 @@ function App() {
     </>
   );
 }
-
 export default App;

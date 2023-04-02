d9c64a1a9ee0a21a499797d70fd5648b48764dd7
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-04-02 10:45:46 +0530

    Updated new validation for forgot password

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ import { comparePassword, hashPassword } from './../helpers/authHelper.js';
 import JWT from "jsonwebtoken";
 export const registerController = async(req,res) => {
     try{
-        const {name,email,password,phone,address} = req.body
+        const {name,email,password,phone,address,answer} = req.body;
 
         // validations
         if(!name){
@@ -21,6 +21,9 @@ export const registerController = async(req,res) => {
         if(!address){
             return res.send({message:"Address is required"})
         }
+        if(!answer){
+            return res.send({message:"Answer is required"})
+        }
 
         // check user
         const existingUser = await userModel.findOne({email})
@@ -30,14 +33,20 @@ export const registerController = async(req,res) => {
             return res.status(200).send({
                 success:false,
                 message:"Already registered please login",
-            })
+            });
         }
 
         // register user
-        const hashedPassword = await hashPassword(password)
+        const hashedPassword = await hashPassword(password);
 
         // to save
-        const user = await new userModel({name,email,phone,address,password:hashedPassword}).save();
+        const user = await new userModel({
+            name,
+            email,
+            phone,
+            address,
+            password:hashedPassword,
+            answer}).save();
 
         res.status(201).send({
             success:true,
@@ -64,7 +73,7 @@ export const loginController = async(req,res) => {
         if(!email || !password){
             return res.status(404).send({
                 success:false,
-                message:"Invalid email or password"
+                message:"Invalid email or password",
             });
         }
 
@@ -73,7 +82,7 @@ export const loginController = async(req,res) => {
         if(!user){
             return res.status(404).send({
                 success:false,
-                messae:"Email is not registered"
+                message:"Email is not registered",
             });
         }
         const match = await comparePassword(password,user.password)
@@ -81,7 +90,7 @@ export const loginController = async(req,res) => {
         if(!match){
             return res.status(200).send({
                 success:false,
-                messae:"Invalid password"
+                message:"Invalid password",
             })
         }
 
@@ -109,6 +118,48 @@ export const loginController = async(req,res) => {
     }
 };
 
+// Forgot password
+export const forgotPasswordController = async(req,res) => {
+    try {
+        const {email,answer,newPassword} = req.body;
+        if(!email){
+            res.status(400).send({message:'Email is required'});
+        }
+        if(!answer){
+            res.status(400).send({message:'Answer is required'});
+        }
+        if(!newPassword){
+            res.status(400).send({message:'New Password is required'});
+        }
+
+        // to check
+        const user = await userModel.findOne({email,answer});
+
+        // validation
+        if(!user){
+            return res.status(404).send({
+                success:false,
+                message:'Wrong Email or Answer',
+            });
+        }
+
+        const hashed = await hashPassword(newPassword);
+        await userModel.findByIdAndUpdate(user._id,{password:hashed});
+        res.status(200).send({
+            success:true,
+            message:'Password Reset Successfully',
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success:false,
+            message:'Something went wrong',
+            error,
+        })
+    }
+};
+
+
 // test controller
 export const testController = (req,res) => {
     try{
@@ -117,5 +168,5 @@ export const testController = (req,res) => {
         console.log(error);
         res.send({error});
     }
-}
+};
 

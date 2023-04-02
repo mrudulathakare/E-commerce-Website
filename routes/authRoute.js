b2904f325f2a7d60342669e83056cab945c2a53c
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-04-02 10:40:32 +0530

    Created new path for forgot password

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {registerController, loginController, testController} from '../controllers/authController.js'
+import {registerController, loginController, testController, forgotPasswordController} from '../controllers/authController.js'
 import {requireSignIn, isAdmin} from "../middlewares/authMiddleware.js";
 
 // router object 
@@ -12,11 +12,14 @@ router.post('/register',registerController)
 // LOGIN || POST
 router.post('/login',loginController)
 
+// FORGOT PASSWORD || POST
+router.post('/forgot-password',forgotPasswordController);
+
 // test routes
 router.get('/test',requireSignIn, isAdmin, testController)
 
 // protected route auth
-router.get('/user/auth',requireSignIn,(req,res)=>{
+router.get('/user-auth',requireSignIn,(req,res)=>{
     res.status(200).send({ok:true});
 });
-export default router
+export default router;

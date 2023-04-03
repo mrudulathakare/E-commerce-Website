103e93ff18dd4af6200b8bbea29e7c5626eee38b
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-04-03 18:20:45 +0530

    Route for category API

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
 import authRoutes from './routes/authRoute.js';
+import cors from 'cors';
+import categoryRoutes from './routes/categoryRoutes.js';
 
 // configure env
 dotenv.config();
@@ -15,16 +17,18 @@ connectDB();
 const app = express()
 
 // middleware
+app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
 
 // routes
 app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/category',categoryRoutes);
 
 // rest api
 app.get('/',(req,res) => {
-    res.send("<h1>Welcome to e-commerce app</h1>")
-})
+    res.send("<h1>Welcome to i2Eye</h1>")
+});
 
 // port
 const PORT = process.env.PORT || 8080;
@@ -32,5 +36,4 @@ const PORT = process.env.PORT || 8080;
 // listen()
 app.listen(PORT,() => {
     console.log(`Server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white);
-}
-)
+});

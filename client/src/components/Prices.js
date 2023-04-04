1f0785551fc5fa1325326e341d51936d82f36a91
Nirali Mamaniya <niralimamaniya29@gmail.com>
2023-04-04 23:07:25 +0530

    Added prices filter for products

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "100 to 499",
+      array: [100, 499],
+    },
+    {
+      _id: 1,
+      name: "500 to 999",
+      array: [500, 999],
+    },
+    {
+      _id: 2,
+      name: "1000 to 2999",
+      array: [1000, 2999],
+    },
+    {
+      _id: 3,
+      name: "3000 to 4999",
+      array: [3000, 4999],
+    },
+    {
+      _id: 4,
+      name: "5000 to 6999",
+      array: [5000, 6999],
+    },
+    {
+      _id: 4,
+      name: "7000 or more",
+      array: [7000, 9999],
+    },
+  ];
